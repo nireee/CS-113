60318be9dcea72797629c17dfe5395f1cdb0805b
Steve Zhao <steve14c@gmail.com>
2021-11-02 15:13:51 -0700

    map 1

--- 113 Puzzle Game/Assets/Scripts/Blocks.cs
+++ 113 Puzzle Game/Assets/Scripts/Blocks.cs
@@ -4,27 +4,81 @@ using UnityEngine;
 
 public class Blocks : MonoBehaviour
 {
-    public bool Movable;
+    GameObject player;
+    bool moving = false;
+    Vector2 startPos;
+    Vector2 destPos;
+    float duration = .0f;
+    float iceSpeed = 10.0f;
+    
+    public Rigidbody2D rb;
+
+    Vector2[] directions = { new Vector2(1, 0), new Vector2(0, 1), new Vector2(-1, 0), new Vector2(0, -1) };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(moving)
+        {
+            moveWord();
+        }
     }
 
+    public void moveIce(Vector2 direction)
+    {
+        Vector2 movement = directions[(((int)Mathf.Round(Mathf.Atan2(direction.y, direction.x) / (2 * Mathf.PI / 4))) + 4) % 4];
+        rb.velocity = movement * iceSpeed;
+    }
 
-    //block as wall
+    public void setupWordMovement(Vector2 direction)
+    {
+        Collider2D[] res = new Collider2D[5];
+        if(rb.OverlapCollider(new ContactFilter2D(), res) > 1)
+            return;
+        Vector2 movement = directions[(((int)Mathf.Round(Mathf.Atan2(direction.y, direction.x) / (2 * Mathf.PI / 4))) + 4) % 4];
+        moving = true;
+        startPos = transform.position;
+        destPos = startPos + movement;
+        player.GetComponent<PlayerController>().stopPlayerMovement();
+    }
+
+    private void moveWord()
+    {
+        rb.MovePosition(Vector2.Lerp(startPos, destPos, duration));
+        duration += Time.deltaTime;
+        if (duration >= 1.0f)
+        {
+            transform.position = destPos;
+            moving = false;
+            duration = 0.0f;
+            player.GetComponent<PlayerController>().startPlayerMovement();
+        }
+    }
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Block Collision");
+        /*if (collision.gameObject.name == "Player")
+        {
+            return;
+        }
+        Debug.Log("Reset Velocity");
+        rb.velocity = Vector3.zero;
+        rb.
+    }*/
+
+        //block as wall
 
 
-    //block as word blocks
+        //block as word blocks
 
 
-    
-    
-}
+
+
+    }
--- 113 Puzzle Game/Assets/Scripts/PlayerController.cs
+++ 113 Puzzle Game/Assets/Scripts/PlayerController.cs
@@ -1,83 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class PlayerController : MonoBehaviour
-{
-    
-    public Transform follow_point;
-    public GameObject player;
-    public Transform portal_target;
-    public Transform portal_start;
-    public Transform portal_end;
-    public float moveSpeed = 2;
-    public bool MeetNPC;
-    public bool portal_state;
-    public GameObject NPC;
+{ 
 
+    public float moveSpeed = 5;
 
-    public bool hasHeatStone;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        hasHeatStone = false;
-        MeetNPC = false;
-        portal_state = false;
-    }
+    bool moveable = true;
+    bool hasFlameTorch = false;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
+        if (!moveable)
+            return;
+        //basic movement script
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(KeyCode.E) && MeetNPC)
-        {
-            //provide a logic question to the player and give hint
-            MeetNPC = false;
-            NPC.GetComponent<Collider2D>().enabled = false;
-        }
-
-        ChangePortalState();
-
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    private void ChangePortalState()
+    public void stopPlayerMovement()
     {
-        if (portal_state)
-        {
-            portal_target.position = new Vector2(portal_start.position.x, portal_start.position.y);
-        }
-        else if (!portal_state)
-        {
-            portal_target.position = new Vector2(portal_end.position.x, portal_end.position.y);
-        }
-        
+        moveable = false;
+        animator.SetFloat("Horizontal", 0);
+        animator.SetFloat("Vertical", 0);
+        animator.SetFloat("Speed", 0);
+    }
 
+    public void startPlayerMovement()
+    {
+        moveable = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!moveable)
+            return;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "NPC")
+        Debug.Log("OnCollisionEnter2D");
+        if (collision.gameObject.transform.parent.name == "Ice")
         {
-            MeetNPC = true;
+            if(hasFlameTorch)
+            {
+                collision.gameObject.SetActive(false);
+            }
+            // collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Vector2 p = rb.position;
+            Vector2 direction = (Vector2) collision.gameObject.transform.position - p;
+            collision.gameObject.GetComponent<Blocks>().moveIce(direction);
+            return;
         }
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Goal")
+        if (collision.gameObject.transform.parent.parent.name == "Word")
         {
-            Debug.Log("You Win!");
-            player.GetComponent<Rigidbody2D>().isKinematic = true;
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            // collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Vector2 p = rb.position;
+            Vector2 direction = (Vector2)collision.gameObject.transform.position - p;
+            collision.gameObject.GetComponent<Blocks>().setupWordMovement(direction);
+            return;
         }
-        if(collision.tag == "Portal")
-        {
-            player.transform.position = new Vector2(portal_target.position.x, portal_target.position.y);
-            portal_state = !portal_state;
-        }
-    }
-    
 
+        if (collision.gameObject.transform.parent.name == "PF Grass A")
+        {
+            Debug.Log("OnCollisionEnter2DFlameTorch");
+            hasFlameTorch = true;
+            collision.gameObject.SetActive(false);
+        }
+
+    }
 }
